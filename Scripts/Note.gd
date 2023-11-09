fde28ccfad0f6519f2fa2d8c21a6dd0c0dbf34bf
rlett <ral109@pitt.edu>
2023-11-09 15:39:26 -0500

    fixed small bug with notes position

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -5,9 +5,9 @@ var go = false
 
 func _startUp(speed, pos):
 	noteSpeed = speed
-	position.x = 660 + 200 * pos
+	self.position.x = 660 + 200 * pos
 	# 90 is destination. ALWAYS TAKE 2 SECONDS FROM SPAWN TO HIT
-	position.y = 90 + speed*2
+	self.position.y = 90 + speed*2
 
 # Is no go? GO! Is go? NO GO!
 func _toggleMove():
@@ -16,10 +16,10 @@ func _toggleMove():
 # move
 func _process(delta):
 	if(go):
-		position.y -= noteSpeed * delta
+		self.position.y -= noteSpeed * delta
 		# save da world
 		# my final message.
-		if(position.y < -100):
+		if(self.position.y < -100):
 			# goodb ye
 			self.queue_free()
 			
